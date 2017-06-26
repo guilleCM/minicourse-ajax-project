922be304df62cff5de0734af2d5bbcdff8c314c6
Guillermo Cirer <guillermo.cirer@ETOOL.TECH>
2017-06-26 12:53:08 +0200

    añadida imagen de fondo segun el input form, añadidos link y titulos del NYT

--- js/script.js
+++ js/script.js
@@ -12,10 +12,35 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
-
+    var street=$('#street').val();
+    var city=$('#city').val();
+    $greeting.text('So, you want to live at '+street+', '+city+'?')
+    $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+street+','+city+'">');
+    
+    // load NYT api
+        //$.getJSON es equivalente a:
+        // $.ajax({
+        //   dataType: "json",
+        //   url: url,
+        //   data: data,
+        //   success: success
+        // });
+    var NYT_URL='https://api.nytimes.com/svc/search/v2/articlesearch.json';
+    var NYT_KEY='be57cd92c57d429a9d7c9444b6452453';
+    var URL=NYT_URL+'?q='+city+'&api-key='+NYT_KEY;
+    $.getJSON(URL, function(data) {
+        console.log(data);
+        var articles = [];
+        $.each(data["response"]["docs"], function() {
+            var title = (this)["headline"].main;
+            var link = '<a href='+(this)["web_url"]+'>'+title+'</a>';
+            var parr = '<p> parrafo </p>'
+            articles.push('<li class="article">'+link+parr+'</li>');
+        });
+        $nytElem.html(articles.join(""));
+    });
     return false;
+
 };
 
 $('#form-container').submit(loadData);
